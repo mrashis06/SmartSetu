7768790113d6447d3300a8a058bf64b00e7d36ab
Ashis Kumar Rai <ashisrai9865@gmail.com>
2025-08-16 13:15:01 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  projectId: "smartsetu-landing",
-  appId: "1:1035058494680:web:1c9277c1c3e691e4e3abad",
-  storageBucket: "smartsetu-landing.firebasestorage.app",
-  apiKey: "AIzaSyDOH8fizsMCyagh-I-gKPVp57nmmbcUcNo",
-  authDomain: "smartsetu-landing.firebaseapp.com",
-  messagingSenderId: "1035058494680",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
